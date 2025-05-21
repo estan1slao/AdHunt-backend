7125e2aedc7a470ee8240c158d4d10b63ec621ca
estan1slao <stas_latypov@mail.ru>
2025-05-21 19:04:47 +0500

    (!) модель

--- AdHunt_backend/api/models.py
+++ AdHunt_backend/api/models.py
@@ -1,33 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.db import models
 
 class Role(models.TextChoices):
     USER = 'user', 'User'
     MODERATOR = 'moderator', 'Moderator'
 
-class CustomUser(AbstractUser):
-    username = None  # Убираем поле username
-    email = models.EmailField(_('email address'), unique=True)
-    role = models.CharField(
-        max_length=10,
-        choices=Role.choices,
-        default=Role.USER,
-    )
-    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
-    first_name = models.CharField(max_length=150, verbose_name='Имя')
-    middle_name = models.CharField(max_length=150, blank=True, null=True, verbose_name='Отчество')
-    phone_number = models.CharField(max_length=15, unique=True, verbose_name='Номер телефона')
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=15)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'phone_number']
 
-    class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-
     def __str__(self):
-        return self.email
+        return self.emai
 
 class AdvertisementStatus(models.TextChoices):
     PENDING = 'pending', 'На модерации'

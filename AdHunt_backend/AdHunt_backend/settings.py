d7bed4e6b9b1e91e9d9941462109263a8994a667
estan1slao <stas_latypov@mail.ru>
2025-05-22 22:40:22 +0500

    (!) env

--- AdHunt_backend/AdHunt_backend/settings.py
+++ AdHunt_backend/AdHunt_backend/settings.py
@@ -4,6 +4,8 @@ import os
 from pathlib import Path
 from datetime import timedelta
 import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 

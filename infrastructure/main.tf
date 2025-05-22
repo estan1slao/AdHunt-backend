f54f39f977fade5f6ff6d12d8da80e72cf64125a
estan1slao <stas_latypov@mail.ru>
2025-05-22 19:01:14 +0500

    (!) что-то поменялось

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.89"
+    }
+  }
+}
+
 provider "yandex" {
   token     = var.yc_token
   cloud_id  = var.cloud_id
